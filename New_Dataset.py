0d2d3877f2672050da605f90f9226574504aaedf
MukulLambat <mukul04lambat@gmail.com>
2024-06-18 11:12:04 +0200

    Final Dataset Created

--- New_Dataset.py
+++ New_Dataset.py
@@ -8,7 +8,7 @@ import glob
 import re
 #%%
 Raw_Data_path = '/Users/mukul/Desktop/DLR_Internship/Data/Raw_Data'
-
+ 
 # Define a custom sorting key function
 def extract_number(filename):
     # Use regular expression to extract the number from the filename
@@ -72,3 +72,24 @@ for file in Sorted_Acceleration_Files:
     Dataset.to_csv(f'/Users/mukul/Desktop/DLR_Internship/Data/Combined_Load_&_Force/Combined_{Split_File_Name[0]}_{Split_File_Name[1]}_Force.csv', index=False)
 
 # %% Now Concatnate each individual CSV to create the whole Dataset
+Path_Combine_Individual_Simulation_Result = ('/Users/mukul/Desktop/DLR_Internship/Data/Combined_Load_&_Force')
+
+# Get the List of aceeleration files in a list using glob
+Individual_Simulation_Result = glob.glob(os.path.join(Path_Combine_Individual_Simulation_Result, 'Combined_Load_*_Force.csv'))
+
+# Sort the files based on the number present in the filename
+Sorted_Simulation_Result = sorted(Individual_Simulation_Result, key=extract_number)
+
+# Empty list to store Individual Dataframe
+Simulation_Result = []
+
+for Each_Simulation in Sorted_Simulation_Result:
+    df = pd.read_csv(Each_Simulation)
+    Simulation_Result.append(df)
+    
+# Concatenate all DataFrames along axis 0 (vertically)
+Concatenated_Simulation_Result = pd.concat(Simulation_Result, axis=0, ignore_index=True)
+
+# Save the concatenated DataFrame to a new CSV file
+
+Concatenated_Simulation_Result.to_csv('/Users/mukul/Desktop/DLR_Internship/Data/Combine_Data.csv', index=False)
